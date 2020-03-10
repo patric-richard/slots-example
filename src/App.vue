6facdb5452390d256359596bbbb2f94c4bb64863
Patric Lundin <patric.lundin@sll.se>
2020-03-10 23:56:35 +0100

    Add small example app using slots

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="flex-layout">
+      <!-- #0 Default without slots -->
+      <Container />
+
+      <!-- #1 & #2 Change header/text -->
+      <Container title="Changed title" />
+      <Container text="Changed text" />
+
+      <!-- #3 Need to change header to h1-->
+      <Container>
+        <template #header>
+          <h1>H1 title</h1>
+        </template>
+      </Container>
+
+      <!-- #4 Need to change to h1 but use default title -->
+      <Container>
+        <template #header="{ title }">
+          <h1>{{ title }}</h1>
+        </template>
+      </Container>
+
+      <!-- #5 Change main content  -->
+      <Container title="Dual Layout">
+        <template #content>
+          <DualLayout />
+        </template>
+      </Container>
+
+      <!-- #6 Change main content nested....  -->
+      <Container title="Dual Layout 2">
+        <template #content="{ text }">
+          <DualLayout :rightText="text">
+            <template #left>
+              <DualLayout leftText="Nested left dual layout" />
+            </template>
+          </DualLayout>
+        </template>
+      </Container>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import Container from './components/Container.vue';
+import DualLayout from './components/DualLayout.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    Container,
+    DualLayout,
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  background-color: #333;
+}
+
+h1, h2, h3, h4, h5, h6, p {
+  margin: 0;
+}
+
+.flex-layout {
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
